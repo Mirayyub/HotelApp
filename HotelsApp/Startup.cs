4d260b897616b7155e6c9e5f06451087aad71db0
mirayyub <mirayyubab@code.edu.az>
2019-09-09 02:23:24 +0400

    Add Register

---
+++ HotelsApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HotelsApp.Startup))]
+namespace HotelsApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
