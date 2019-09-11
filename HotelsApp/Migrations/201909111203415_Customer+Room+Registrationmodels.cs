cd147d07ee0ed72556aadbbb469d07e785b61f4f
mirayyub <mirayyubab@code.edu.az>
2019-09-11 16:13:12 +0400

    Update room, customer, register model

---
+++ HotelsApp/Migrations/201909111203415_Customer+Room+Registrationmodels.cs
@@ -0,0 +1,16 @@
+namespace HotelsApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CustomerRoomRegistrationmodels : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
