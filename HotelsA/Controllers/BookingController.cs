e10fb511e031727868b307d6aaeee6b514564657
mirayyub <mirayyubab@code.edu.az>
2019-09-17 18:00:04 +0400

    create booking controller

---
+++ HotelsA/Controllers/BookingController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelsA.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly HotelsAContext _context;
+
+        public BookingController()
+        {
+            _context = new HotelsAContext();
+        }
+        // GET: Booking
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
